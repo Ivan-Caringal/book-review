bf719dfd9cb59709d6647538e3854e6486260a5b
Ivan-Caringal <ivancaringal20@gmail.com>
2025-05-21 22:07:28 +0800

    feat : added a add review feature

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
+use App\Models\Book;
 
 
 Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
+    
+    
